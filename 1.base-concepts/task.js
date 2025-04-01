60942587f2b93a1a95ce17e01f89fc717e2e1234
Dmitry Leonov <lemomonster@mail.ru>
2025-04-01 21:15:52 +0500

    ДЗ готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"USE STRICT"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant < 0) {
+
+		return [];
+	} else if (discriminant === 0) {
+		return [-b / (2 * a)];
+	} else {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+
+	}
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	monthPay = percent / 100 / 12;
+	creditBody = amount - contribution;
+	let pay = creditBody * (monthPay + (monthPay / (((monthPay + 1) ** countMonths) - 1)))
+	paySum = pay * countMonths
+	paySum = Number(paySum.toFixed(2))
+
+	return paySum;
+
 }
